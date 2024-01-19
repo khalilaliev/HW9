581fdde3ac81745d01801c7e152c4c113bfbf980
Khalil <khalilaliev0@gmail.com>
2024-01-19 02:52:25 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const firstInput = prompt('Enter first number:');
+const seconfInput = prompt('Enter second number:');
+const thirdInput = prompt('Enter third number:');
+
+const digitString = `Result: ${firstInput} ${seconfInput} ${thirdInput}`;
+console.log('Result', digitString)
+
+
+const fiveDigitNumber = prompt('Enter the five digit number:');
+const splitDigitNumber = fiveDigitNumber.toString().split('').join(' ');
+console.log('Five digit Number:', splitDigitNumber);
+
+
+
+
+// const avarageNumber = () => {
+//     const firstInput = prompt('Enter first number:');
+//     const seconfInput = prompt('Enter second number:');
+//     const thirdInput = prompt('Enter third number:');
+
+//     const digitString = `${firstInput} ${seconfInput} ${thirdInput}`;
+//     const three = 3;
+//     const div = digitString * three;
+
+//     alert('Result', div);
+
+// }
+// avarageNumber()
+
